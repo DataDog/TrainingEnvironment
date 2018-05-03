d4301a7ab9297a81d959d18385e96089f73f50ca
technovangelist <mattw@datadoghq.com>
2018-05-03 08:52:58 -0400

    install to current dir

--- Datadog101/install.sh
+++ Datadog101/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-installdir="$HOME/DatadogTrainingEnvironment"
+installdir=$PWD
 
 
 if [ -n "$1" ]; then
@@ -20,43 +20,21 @@ if [ ! $(command -v curl) ]; then
     exit 1;
 
 fi
-printf "This script will download the Training Environment. By default 
-the environment will be installed in:\n\n    $installdir\n\n
-If this is OK, press Enter here. Otherwise type the directory you 
-would prefer to use.\n\033[31mInstall the environment in [$installdir]\033[0m: "
-read newinstalldir
-workingdir=0
-while [ $workingdir -ne 1 ]; do
-  newinstalldir=${newinstalldir/"~"/$HOME}
-  if ((${#newinstalldir} > 0));then
-    if [ -d $newinstalldir ]; then
-      installdir=$newinstalldir
-      workingdir=1
-    else
-      printf "That directory doesn't exist. Try again, or press enter to accept the default of \n\n    $installdir\n\033[31mInstall the environment in [$installdir]\033[0m: "
-      read newinstalldir
-    fi
-  else 
-    workingdir=1
-    if [ ! -d $installdir ]; then
-      printf "\033[31mCreating the directory: $installdir\033[0m\n"
-      mkdir $installdir
-    fi
-  fi
-done
+printf "This script will download the Training Environment. It is installed into the current directory:\n\n    $installdir\n\n
+Feel free to move this to a different directory, but be sure to \n
+run the update.sh script to update the location of the API key definition file.\033[0m"
 
-origwd=$PWD
-cd $installdir
 
 printf "\033[31mDownloading the Training Environment from Github \033[0m\n"
-curl -L -J -O https://github.com/DataDog/TrainingEnvironment/archive/master.zip
+outputfilename=TrainingEnvironment-$(date +%m%Y%d)
+curl -L -J https://github.com/DataDog/TrainingEnvironment/archive/master.zip -o $outputfilename
 printf "\033[31mUnzipping the Training Environment \033[0m\n"
-unzip -q TrainingEnvironment-master.zip
-mv TrainingEnvironment-master/* .
-rm TrainingEnvironment-master/.gitignore
-rmdir TrainingEnvironment-master
+unzip -q $outputfilename
+mv TrainingEnvironment-master/Datadog101/* .
+rm -rf TrainingEnvironment-master
+
 printf "\033[31mConfiguring... \033[0m\n"
-sed -i "" "s|source: '~/.ddtraining.sh'|source: '$PWD/.ddtraining.sh'|g" Datadog101/Vagrantfile
+sed -i "" "s|source: '~/.ddtraining.sh'|source: '$installdir/.ddtraining.sh'|g" Datadog101/Vagrantfile
 printf "#!/bin/bash\nDD_API_KEY='$apikey'\n"> .ddtraining.sh
 
 if [ ! $(command -v vagrant) ]; then
@@ -64,5 +42,4 @@ if [ ! $(command -v vagrant) ]; then
 fi
 printf "Installation complete\nReturn to $installdir whenever you need to run the Vagrant-based environment. \nThe key commands to remember are: \n\n\033[31mvagrant up\033[0m - launches the vagrant environment\n\033[31mvagrant halt\033[0m - stops the vagrant environment\n\033[31mvagrant destroy\033[0m - destroys the vagrant environment, but a vagrant up brings it all back\n\nI often run the single line: \033[31mvagrant halt;vagrant destroy -f;vagrant up\033[0m to restart everything."
 
-cd $origwd
 
