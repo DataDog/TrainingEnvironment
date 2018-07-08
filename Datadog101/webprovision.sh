42b42f2e0541ff73d5b7ae4dcdcacbc8cb0ee0b7
Matt Williams <technovangelist@gmail.com>
2018-07-08 16:59:32 -0400

    Update webprovision.sh

--- Datadog101/webprovision.sh
+++ Datadog101/webprovision.sh
@@ -18,9 +18,10 @@ echo gethostname();\n?></strong></div>\n<br>" | sudo tee /var/www/html/index.php
 
 less /etc/apache2/apache2.conf
 #printf "\nExtendedStatus ON" >> /etc/apache2/apache2.conf
-printf "\nExtendedStatus ON" | sudo tee /etc/apache2/apache2.conf
+#printf "\nExtendedStatus ON" | sudo tee /etc/apache2/apache2.conf
+sudo sh -c '\nExtendedStatus ON >/etc/apache2/apache2.conf'
 #printf "\n<Location /server-status>\n\tSetHandler server-status\n\tRequire all granted\n</Location>\n" >> /etc/apache2/apache2.conf
-printf "\n<Location /server-status>\n\tSetHandler server-status\n\tRequire all granted\n</Location>\n" | sudo tee /etc/apache2/apache2.conf
+#printf "\n<Location /server-status>\n\tSetHandler server-status\n\tRequire all granted\n</Location>\n" | sudo tee /etc/apache2/apache2.conf
 #sudo systemctl restart apache2.service
 sudo service apache2 stop
 sudo service apache2 start
