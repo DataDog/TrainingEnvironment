de1d0966a8e3e6124ee1aa2d03da9e8f95b0959d
technovangelist <mattw@datadoghq.com>
2018-09-13 17:11:06 -0400

    Added current v of apm

---
+++ apm/step01/api.py
@@ -0,0 +1,22 @@
+import requests
+
+from flask import Flask, Response, jsonify
+from flask import request as flask_request
+
+from ddtrace import tracer
+from ddtrace.contrib.flask import TraceMiddleware
+
+
+# Tracer configuration
+tracer.configure(hostname='agent')
+
+app = Flask('api')
+traced_app = TraceMiddleware(app, tracer, service='thinker-api')
+
+
+@app.route('/think/')
+def think_handler():
+    thoughts = requests.get('http://thinker:5001/',headers={'x-datadog-trace-id/':str(tracer.current_span().trace_id), 'x-datadog-parent-id': str(tracer.current_span().span_id), },  params={
+        'subject': flask_request.args.getlist('subject', str),
+    }).content
+    return Response(thoughts, mimetype='application/json')
---
+++ apm/step01/thinker.py
@@ -0,0 +1,36 @@
+import requests
+
+from flask import Flask, Response, jsonify
+from flask import request as flask_request
+
+from ddtrace import tracer
+from ddtrace.contrib.flask import TraceMiddleware
+
+from thoughts import thoughts
+from time import sleep
+
+# Tracer configuration
+tracer.configure(hostname='agent')
+
+app = Flask('api')
+traced_app = TraceMiddleware(app, tracer, service='thinker-microservice')
+
+@tracer.wrap(name='think')
+def think(subject):
+    tracer.current_span().set_tag('subject', subject)
+
+    sleep(0.5)
+    return thoughts[subject]
+
+@app.route('/')
+def think_microservice():
+    trace_id = flask_request.headers.get("X-Datadog-Trace-Id")
+    parent_id = flask_request.headers.get("X-Datadog-Parent-Id")
+    if trace_id and parent_id:
+      span=tracer.current_span()
+      span.trace_id=int(trace_id)
+      span.parent_id=int(parent_id)
+      
+    subject = flask_request.args.get('subject')
+    thoughts = think(subject)
+    return Response(thoughts, mimetype='application/json')
