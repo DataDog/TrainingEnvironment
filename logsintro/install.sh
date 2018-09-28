a9867fde737ac04e3b61ea69a547c46bd6bb0858
Gus <pierre.guceski@datadoghq.com>
2018-09-28 13:59:42 +0200

    Adds a log posting to enable DD log Mgt
    Adds a log posting after installing the local env for the course in order to enable Datadog Log Management solution.

--- logsintro/install.sh
+++ logsintro/install.sh
@@ -35,6 +35,11 @@ printf "\033[31mConfiguring... \033[0m\n"
 
 printf "DD_API_KEY=$apikey\n"> apikey.env
 
+printf "\033[31mSetting up Datadog Log Managment solution... \033[0m\n"
+
+exec 3<>/dev/tcp/intake.logs.datadoghq.com/10514
+echo "$apikey {\"hostname\": \"datadog\", \"level\":\"info\", \"ddtags\":\"key:value,env:learning\",\"ddsource\": \"instal_script\", \"message\": \"Welcome to Datadog Log Management Solution, click me!\", \"facet\": \"facet_value\", \"measure\": 1234}" 1>&3
+
 # if [ ! $(command -v docker-compose) ]; then
 #     printf "You will need to install Docker and docker-compose to get the system up and running.\nGo to https://www.docker.com/get-started for more on doing that."
 # fi
