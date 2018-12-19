b33b3a3174a43ba1f0ecd9fa28fb548438a87ddc
technovangelist <mattw@datadoghq.com>
2018-12-19 17:59:19 -0500

    grr

--- logsparsing/provision.sh
+++ logsparsing/provision.sh
@@ -37,4 +37,5 @@ sudo chmod 666 /var/run/docker.sock
 sudo chmod +s /usr/sbin/tcpdump
 sudo chmod +x /vagrant/setupagent.sh
 
-printf "cd /vagrant\ndocker-compose up\n" > /home/vagrant/.bashrc
+echo "cd /vagrant" >> /home/vagrant/.bashrc
+echo "docker-compose up" >> /home/vagrant/.bashrc
